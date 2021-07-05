a36fd447b72a15ce4e79e3bee729f8975d79b5a1
Gabriel Barbosa <GabrielBGasparV@gmail.com>
2021-07-05 19:59:47 -0300

    New action routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,3 +1,8 @@
+import { countryGetAllAction } from "./controller/CountryGetAllAction";
+import { countySaveAction } from "./controller/CountySaveAction";
+import { countyWithCountryAndDistrictGetAllAction } from "./controller/CountyWithCountryAndDistrictGetAllAction";
+import { countyWithCountryAndDistrictGetByIdAction } from "./controller/CountyWithCountryAndDistrictGetByIdAction";
+import { districtGetByCountryIdAction } from "./controller/DistrictGetByCountryIdAction";
 import {postGetAllAction} from "./controller/PostGetAllAction";
 import {postGetByIdAction} from "./controller/PostGetByIdAction";
 import {postSaveAction} from "./controller/PostSaveAction";
@@ -20,5 +25,30 @@ export const AppRoutes = [
         path: "/posts",
         method: "post",
         action: postSaveAction
+    },
+    {
+        path: "/countries",
+        method: "get",
+        action: countryGetAllAction
+    },
+    {
+        path: "/countiesWithCountryAndDistrict",
+        method: "get",
+        action: countyWithCountryAndDistrictGetAllAction
+    },
+    {
+        path: "/district/byCountry/:countryId",
+        method: "get",
+        action: districtGetByCountryIdAction
+    },
+    {
+        path: "/countyWithCountryAndDistrict/:id",
+        method: "get",
+        action: countyWithCountryAndDistrictGetByIdAction
+    },
+    {
+        path: "/county",
+        method: "post",
+        action: countySaveAction
     }
 ];
